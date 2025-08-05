a7359eb598cabaf5c1e52eda14ba9cdf043cf475
Hossein <mh.amiri.notash@gmail.com>
2025-08-05 19:11:58 +0330

    create customer and user models

---
+++ Backend/app/Models/Customer.php
@@ -0,0 +1,16 @@
+<?php   
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model {
+    protected $table = "customer_tbl";
+    protected $fillable = [
+        'UserId','FirstName','LastName','national_id','phone','DateOfBirth','address'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}

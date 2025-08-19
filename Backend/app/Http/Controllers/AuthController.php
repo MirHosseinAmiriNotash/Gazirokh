3e66c24c8df6292ad1931aa65b05eb6fb3f32c08
Hossein <mh.amiri.notash@gmail.com>
2025-08-19 21:49:30 +0330

    feat : Add Methods for send otp code and change password

--- Backend/app/Http/Controllers/AuthController.php
+++ Backend/app/Http/Controllers/AuthController.php
@@ -154,5 +154,85 @@ class AuthController extends Controller{
             'user' => $user
         ]);
     }//loginWithPassword
+
+
+    public function forgotPasswordRequestOtp(Request $request){
+        $request->validate([
+            'phone' => 'required|regex:/^09[0-9]{9}$/'
+        ]);
+
+        $user = User::where('phone',$request->phone)->first();
+        if(!$user){
+            return response()->json(['message'=>'کاربری با این شماره پیدا نشد'],404);
+        }
+        $otp = rand(100000,999999);
+        $expiredAt = Carbon::now()->addMinutes(2);
+        
+        UserOtp::create([
+            'UserId' => $user->UserId,
+            'phone' => $request->phone,
+            'otp_code' => $otp,
+            'expires_at'=> $expiredAt
+        ]);
+        
+        try{
+            $api =new  KavenegarApi("307976355743335358594D5246445945346C52757759417261585878577532515937434F6A31696F4937493D");
+            $sender = "2000660110";
+            $message = "کد اعتبار سنجی ورود به گزیروخ: {$otp}";
+            $api->Send($sender,$request->phone,$message);
+        } catch(\Exception $e){
+            return response()->json(['message'=> 'خطا در ارسال پیامک','error' => $e->getMessage()],500);
+        }
+
+        return response()->json([
+            'message'=>'کد تایید ارسال شد'
+        ]);
+    }//forgotPasswordRequestOtp
+
+    public function forgotPasswordVerifyOtp(Request $request){
+        $request->validate([
+            'phone' => 'required|regex:/^09[0-9]{9}$/',
+            'otp_code' => 'required|numeric',
+        ]);
+
+         $otpRecord = UserOtp::where('phone', $request->phone)
+            ->where('otp_code',$request->otp_code)
+            ->where('is_used',false)
+            ->where('expires_at','>',now())
+            ->first();
+
+        if(!$otpRecord){
+            return response()->json([
+                'message' => 'کد معتبر نیست یا منقضی شده است'
+            ,400]);
+        }
+
+        $otpRecord->update(['is_used' => true]);
+        
+        return response()->json([
+            'message' => 'کد تایید شد، حالا رمز جدید را وارد کنید',
+            'phone'   => $request->phone
+        ]);
+    }//forgotPasswordVerifyOtp
+
+    public function resetPassword(Request $request){
+
+        $request->validate([
+            'phone' => 'required|regex:/^09[0-9]{9}$/',
+            'password' => 'required|string|min:6',
+        ]);
+        
+        $user = User::where('phone',$request->phone)->first();
+        if(!$user){
+            return response()->json(['message' => 'کاربر یافت نشد'],404);
+        }
+
+        $user->update(['password' => Hash::make($request->password)]);
+
+        return response()->json([
+            'message' => 'رمز عبور با موفقیت تغییر یافت'
+        ]);
+
+    }//resetPassword
     
 }//class
--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -7,4 +7,7 @@ use App\Http\Controllers\AuthController;
 Route::post('/RequestOtp',[AuthController::class,'requestOtp']);
 Route::post('/VerifyOtp',[AuthController::class,'verifyOtp']);
 Route::post('/login',[AuthController::class,'loginWithPassword']);
+Route::post('/forgot-password/request-otp',[AuthController::class,'forgotPasswordRequestOtp']);
+Route::post('/forgot-password/verify-otp',[AuthController::class,'forgotPasswordVerifyOtp']);
+Route::post('/forgot-password/reset',[AuthController::class,'resetPassword']);
 Route::middleware('auth:api')->post('/logout',[AuthController::class,'logout']);
